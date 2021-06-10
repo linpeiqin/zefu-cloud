e7b5e98a58075e49ad26fd731ec201483c70ce19
linpeiqin <286513319@qq.com>
2021-06-10 16:24:48 +0800

    同步测试

--- zefu-modules/zefu-business/src/main/java/com/zefu/business/controller/BusComController.java
+++ zefu-modules/zefu-business/src/main/java/com/zefu/business/controller/BusComController.java
@@ -12,8 +12,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
-
 /**
  * 公共Controller
  *
---
+++ zefu-modules/zefu-gen/src/main/java/com/zefu/gen/ZeFuGenApplication.java
@@ -0,0 +1,25 @@
+package com.zefu.gen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.zefu.common.security.annotation.EnableCustomConfig;
+import com.zefu.common.security.annotation.EnableRyFeignClients;
+import com.zefu.common.swagger.annotation.EnableCustomSwagger2;
+
+/**
+ * 代码生成
+ * 
+ * @author ruoyi|linking
+ */
+@EnableCustomConfig
+@EnableCustomSwagger2   
+@EnableRyFeignClients
+@SpringBootApplication
+public class ZeFuGenApplication
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(ZeFuGenApplication.class, args);
+        System.out.println("(♥◠‿◠)ﾉﾞ  代码生成模块启动成功   ლ(´ڡ`ლ)ﾞ  \n");
+    }
+}
